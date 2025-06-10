ebac1365f2668fd9a60dacae6cec807a6732c5d5
NormanFrieman <ezequiel-d.s@hotmail.com>
2025-06-10 16:14:49 -0300

    fix scanner

--- tests/casetests/loopError.go
+++ tests/casetests/loopError.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
     nums := []int{1, 2, 3}
     // Erro léxico: identificador 'in' não é palavra-reservada nem ID válido no contexto
-    for i in nums {
+    for i # nums {
         fmt.Println(i)
     }
 }
